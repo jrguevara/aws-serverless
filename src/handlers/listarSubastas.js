0251cb948de624f3cac0cdc2b6b780f01805bcda
jr.guevara <jr.guevara@outlook.com>
2023-05-28 04:03:08 -0600

    parte 2

--- src/handlers/listarSubastas.js
+++ src/handlers/listarSubastas.js
@@ -1,17 +1,31 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
 import commonMiddleware from "../../lib/commonMiddleware";
+import validator from "@middy/validator";
+import { transpileSchema } from '@middy/validator/transpile';
 import createError from "http-errors";
+import listarSubastasSchema from "../../lib/schemas/listarSubastasSchema";
 
 const dynamo = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 
 const listarSubastas = async (event, context) => {
     try {
+        const { status } = event.queryStringParameters;
         let subastas;
 
-        const result = await dynamo.send(new ScanCommand({
+        const params = {
             TableName: "SubastasTable",
-        }));
+            IndexName: "statusFechaFinIndex",
+            KeyConditionExpression: '#status = :status',
+            ExpressionAttributeValues: {
+                ':status': status,
+            },
+            ExpressionAttributeNames: {
+                '#status': 'status',
+            },
+        };
+
+        const result = await dynamo.send(new QueryCommand(params));
 
         subastas = result.Items;
 
@@ -32,4 +46,12 @@ const listarSubastas = async (event, context) => {
     }
 };
 
-export const handler = commonMiddleware(listarSubastas);
+export const handler = commonMiddleware(listarSubastas).use(
+    validator({
+        eventSchema : transpileSchema(listarSubastasSchema),
+        ajvOptions: {
+            useDefaults: true,
+            strict: false,
+        },
+    })
+);
--- src/handlers/obtenerSubasta.js
+++ src/handlers/obtenerSubasta.js
@@ -5,15 +5,8 @@ import createError from "http-errors";
 
 const dynamo = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 
-const obtenerSubasta = async (event, context) => {
-
+export async function obtenerSubastaPorId(id) {
     let subasta;
-    const { id } = event.pathParameters;
-    const headers = {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    };
-    
     try {
         const result = await dynamo.send(new GetCommand({
             TableName: "SubastasTable",
@@ -30,6 +23,18 @@ const obtenerSubasta = async (event, context) => {
     if (!subasta) {
         throw new createError.NotFound(`Subasta con id: ${id} no encontrada.`);
     }
+
+    return subasta;
+}
+
+const obtenerSubasta = async (event, context) => {
+    const { id } = event.pathParameters;
+    const subasta = await obtenerSubastaPorId(id);
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    };
 
     return {
         statusCode: 200,
